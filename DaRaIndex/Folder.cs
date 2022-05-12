1bd2ff48c639961bcf5d0ee64dfc7b20d817c16b
Just <jalui@mail.ru>
2022-05-12 20:04:59 +0300

    Folder info class

---
+++ DaRaIndex/Folder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaRaIndex
+{
+    public class Folder
+    {
+        public string Path { get; private set; }
+        public string Date { get; private set; }
+        public int Rate { get; private set; }
+        public bool IsIndexed { get; private set; }
+
+        public Folder(string path, string date, int rate, bool isIndexed)
+        {
+            if (path is null)
+                throw new NullReferenceException(nameof(path));
+
+            if (date is null)
+                throw new NullReferenceException(nameof(date));
+
+            Path = path;
+            Date = date;
+            Rate = rate;
+            IsIndexed = isIndexed;
+        }
+    }
+}
